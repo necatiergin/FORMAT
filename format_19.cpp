fcd04ab31198c884c7409fd19ed87a4b65dc95ea
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-08-18 13:05:18 +0300

    Create format_19.cpp

---
+++ format_19.cpp
@@ -0,0 +1,16 @@
+#include <format>
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	double dval{ 3.14159 / 1.7 };
+	cout << format("|{:12g}|", dval) << '\n';                         // |     1.84799|
+	cout << format("|{:12.2}|", dval) << '\n';                        // |         1.8|
+	cout << format("|{:12e}|", dval) << '\n';                         // |1.847994e+00|
+
+	int width{ 20 };
+	int precision{ 4 };
+	cout << format("|{2:{0}.{1}f}|", width, precision, dval) << '\n'; // |              1.8480|
+}
