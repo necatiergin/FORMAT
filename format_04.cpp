ca997b6cbb4be91baa59abbb76ca033116201dae
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-08-18 12:27:39 +0300

    Create format_04.cpp

---
+++ format_04.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <format>
+
+int main()
+{
+	using namespace std;
+
+	cout << format("{:x}", 47802) << '\n'; // baba 
+	cout << format("{:X}", 47802) << '\n'; // BABA
+	cout << format("{:#x}", 47802) << '\n'; // 0xbaba
+	cout << format("{:#X}", 47802) << '\n'; // 0XBABA
+	cout << format("{:b}", 47802) << '\n'; // 1011101010111010
+	cout << format("{:#b}", 47802) << '\n'; // 0b1011101010111010
+	cout << format("{:o}", 47802) << '\n'; // 135272
+	cout << format("{:#o}", 47802) << '\n'; // 0135272
+	cout << format("{:f}", 5.) << '\n'; //  5.000000
+	cout << format("{:#f}", 5.) << '\n'; // 5.000000
+}
