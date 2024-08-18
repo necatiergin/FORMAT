9adea2f9ac2faa928e9d03bf228160e799847c6a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-08-18 13:03:39 +0300

    Create format_17.cpp

---
+++ format_17.cpp
@@ -0,0 +1,17 @@
+#include <format>
+#include <iostream>
+
+int main()
+{
+	using namespace std;
+
+	int x{ 171 };
+
+	cout << format("|{:20d}|", x) << '\n';   // |                 171|
+	cout << format("|{:20b}|", x) << '\n';   // |            10101011|
+	cout << format("|{:#20b}|", x) << '\n';  // |          0b10101011|
+	cout << format("|{:#20B}|", x) << '\n';  // |          0B10101011|
+	cout << format("|{:20x}|", x) << '\n';   // |                  ab|
+	cout << format("|{:20X}|", x) << '\n';   // |                  AB|
+	cout << format("|{:#20X}|", x) << '\n';  // |                0XAB|
+}
