28283989e9058870308db696abc3001ddb55678a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-08-18 12:32:07 +0300

    Create format_05.cpp

---
+++ format_05.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <format>
+
+int main()
+{
+	using namespace std;
+
+	double dval = 3. / 7;
+	int width = 12;
+	int precision = 5;
+
+	cout << format("|{2:<{0}.{1}f}|", width, precision, dval);    //  |0.42857     |
+	// 2 ==> position
+	// 0 ==> width
+	// 1 ==> precision
+}
