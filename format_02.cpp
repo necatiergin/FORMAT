bd7b2f58ef323042e8883e79b96dc8e07575605e
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-11 10:28:59 +0300

    Update format_02.cpp

--- format_02.cpp
+++ format_02.cpp
@@ -3,19 +3,19 @@
 
 int main()
 {
-	using namespace std;
+	using std::string, std::cout, std::format;
 
 	string name{ "Necati Ergin" };
 	cout << format("|{}|", name) << '\n'; // Necati Ergin
 
 	name = { "murat hepeyiler" };
 	cout << format("{}", name) << '\n'; //	murat hepeyiler
-	cout << format("|{:24s}|", name) << '\n'; // |murat hepeyiler		|   
-	cout << format("|{:<24s}|", name) << '\n'; // |murat hepeyiler		|
-	cout << format("|{:>24s}|", name) << '\n'; // |	     murat hepeyiler|
-	cout << format("|{:^24s}|", name) << '\n'; // |   murat hepeyiler   |
-	cout << format("|{:.^24s}|", name) << '\n'; // |....murat hepeyiler....|
-	cout << format("|{:<24.5s}|", name) << '\n'; // |murat				|
-	cout << format("|{:+>24.5s}|", name) << '\n'; // |+++++++++++++murat|
-	cout << format("|{:!^24.5s}|", name) << '\n'; // |!!!!!!murat!!!!!!|
+	cout << format("|{:24s}|", name) << '\n'; //    |murat hepeyiler		|   
+	cout << format("|{:<24s}|", name) << '\n'; //   |murat hepeyiler		|
+	cout << format("|{:>24s}|", name) << '\n'; //   |	     murat hepeyiler|
+	cout << format("|{:^24s}|", name) << '\n'; //   |    murat hepeyiler    |
+	cout << format("|{:.^24s}|", name) << '\n'; //  |....murat hepeyiler....|
+	cout << format("|{:<24.5s}|", name) << '\n'; // |murat				    |
+	cout << format("|{:+>24.5s}|", name) << '\n'; //|++++++++++++++++++murat|
+	cout << format("|{:!^24.5s}|", name) << '\n'; //|!!!!!!!!!murat!!!!!!!!!|
 }
