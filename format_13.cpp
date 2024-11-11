b4e75d10d7a68bc928d3081afa7317a2ec2c0b3a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-11 10:39:21 +0300

    Update format_13.cpp

--- format_13.cpp
+++ format_13.cpp
@@ -4,18 +4,16 @@
 
 int main()
 {
-	using namespace std;
-
 	int x{ 65234 };
 	double dval = 123.654;
 
 	const char* p{ "{:#X} {:.2f}" };
-	const auto n = formatted_size(p, x, dval); // formatted string size
+	const auto n = std::formatted_size(p, x, dval); // formatted string size
 
-	vector<char> buf(n);
+	std::vector<char> buf(n);
 
 	format_to(buf.begin(), p, x, dval);
 
 	for (auto c : buf)
-		cout << c << ' ';   // 0 X F E D 2   1 2 3 . 6 5
+		std::cout << c << ' ';   // 0 X F E D 2   1 2 3 . 6 5
 }
