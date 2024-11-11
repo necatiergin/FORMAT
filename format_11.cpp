737a47e22d5b270d0826b4c0f259a2d26cef58a6
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-11 10:37:56 +0300

    Update format_11.cpp

--- format_11.cpp
+++ format_11.cpp
@@ -4,25 +4,20 @@
 
 int main()
 {
-	using namespace std;
-
-	string name{ "necati ergin" };
+	std::string name{ "necati ergin" };
 	char buffer[64];
 
 	auto result = format_to_n(buffer, std::size(buffer) - 1, "{} isminin uzunlugu {}.", name, name.length());
 
-	cout << buffer << '\n'; //UB. Not null terminated byte stream.
-
-	cout << result.out << '\n'; // UB.
-
-	cout << result.size << '\n'; // 33
-
-	cout << result.out - buffer << '\n'; // 33
+	std::cout << buffer << '\n';	//UB. Not null terminated byte stream.
+	std::cout << result.out << '\n'; // UB.
+	std::cout << result.size << '\n'; // 33
+	std::cout << result.out - buffer << '\n'; // 33
 
 	//---------------------------------------------------------------------------------------------------------- -
 
 	*result.out = '\0';
 	// buffer[result.size] = '\0';
 
-	cout << "|" << buffer << "|"; //valid
+	std::cout << "|" << buffer << "|"; //valid
 }
